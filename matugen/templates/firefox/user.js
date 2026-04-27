46752eb0f792b78a1785ced38ad0f34409a0978f
John Oberhauser <j.git@obez.io>
2026-04-27 18:51:09 -0500

    firefox user.js update

--- matugen/templates/firefox/user.js
+++ matugen/templates/firefox/user.js
@@ -1,5 +1,7 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("xpinstall.signatures.required", false);
+user_pref("devtools.chrome.enabled", true);
+user_pref("devtools.debugger.remote-enabled", true);
 
 user_pref("font.name.serif.x-western", "{{ okshell.font.primary }}");
 user_pref("font.name.sans-serif.x-western", "{{ okshell.font.primary }}");
